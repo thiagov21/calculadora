7b9d82cec8d31c1f0c583e8c52609a0a07195aeb
Thiago Vieira <77083955+thiagov21@users.noreply.github.com>
2021-01-21 10:43:26 -0300

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,17 @@
+# Calculadora
+
+# Entrada dos números
+primeiro_numero = float(input('Digite o primeiro número: '))
+segundo_numero = float(input('Digite o segundo número: '))
+
+# Operações Matemáticas
+soma = primeiro_numero + segundo_numero
+subtracao = primeiro_numero - segundo_numero
+multiplicacao = primeiro_numero * segundo_numero
+divisao = primeiro_numero / segundo_numero
+
+# Saída dos cálculos
+print(f'A soma entre {primeiro_numero} e {segundo_numero} é {soma}!')
+print(f'A subtração entre {primeiro_numero} e {segundo_numero} é {subtracao}!')
+print(f'A multiplicação entre {primeiro_numero} e {segundo_numero} é {multiplicacao}!')
+print(f'A divisão entre {primeiro_numero} e {segundo_numero} é {divisao}!')
